a867a8f050416b1e59e8bf7d1ada394aa4527983
EgorCharntsev <charncev200447@gmail.com>
2023-11-09 14:00:03 +0300

    add standard classes to project for simple work

---
+++ app/src/main/java/com/itis/homework/base/BaseActivity.kt
@@ -0,0 +1,16 @@
+package com.itis.homework.base
+
+import androidx.appcompat.app.AppCompatActivity
+import com.itis.homework.util.ActionType
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    protected abstract val fragmentContainerId: Int
+
+    abstract fun goToScreen(
+        actionType : ActionType,
+        destination : BaseFragment,
+        key : String? = null,
+        isAddToBackStack : Boolean = true
+    )
+}
---
+++ app/src/main/java/com/itis/homework/base/BaseFragment.kt
@@ -0,0 +1,7 @@
+package com.itis.homework.base
+
+import androidx.annotation.LayoutRes
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment(@LayoutRes layoutRes: Int) : Fragment() {
+}
